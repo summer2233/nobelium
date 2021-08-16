c4dbc7c535d2977704fcedc004ff84fcf8b38d42
summer2233 <xiatianxxx@yeah.net>
2021-08-16 13:12:01 +0800

    Update Socal Link

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   sortByDate: false,
   showAbout: true, // WIP
   showArchive: true, // WIP
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://weibo.com/p/1005055272178938',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
